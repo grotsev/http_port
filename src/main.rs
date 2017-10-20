715294ecd61153486e942dc8aa9c9269f98cbdc1
grotsev <denis.grotsev@gmail.com>
2017-10-20 19:47:13 +0300

    Fix type of notification error from @sfackler

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,20 @@ use futures::{Future, Stream};
 
 fn main() {
     let mut l = Core::new().unwrap();
-    let handle = l.handle();
+    let handle = &l.handle();
 
     let done = Connection::connect(
         "postgres://postgres:111@172.17.0.2:5432",
         TlsMode::None,
         &handle,
     ).then(|c| c.unwrap().batch_execute("LISTEN test_notifications"))
+        .map_err(|(e, _)| e)
         .and_then(|c| {
-            c.notifications()
-                .and_then(|n| {
-                    let serve_one = futures::future::ok(println!("{:?}", n.channel));
-                    handle.spawn(serve_one);
-                    futures::future::ok(n)
-                })
-                .into_future()
-                .map_err(|(e, n)| (e, n.into_inner().into_inner()))
-        })
-        .map(|(n, _)| {
-            let n = n.unwrap();
-            assert_eq!(n.channel, "test_notifications");
-            assert_eq!(n.payload, "foo");
+            c.notifications().for_each(|n| {
+                let serve_one = futures::future::ok(println!("{:?}", n.payload));
+                handle.spawn(serve_one);
+                Ok(())
+            })
         });
 
     l.run(done).unwrap();
